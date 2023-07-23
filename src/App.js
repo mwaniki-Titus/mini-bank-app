83547b44cd9a8140c65f4ecb593935237d388689
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-23 14:02:28 +0300

    complete app.js code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const API_BASE_URL = "http://localhost:8001";
+
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const [newTransaction, setNewTransaction] = useState({
+    description: "",
+    amount: 0,
+    category: "",
+  });
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`${API_BASE_URL}/transactions`)
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewTransaction({ ...newTransaction, [name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setTransactions([...transactions, { ...newTransaction, id: Date.now() }]);
+    setNewTransaction({ description: "", amount: 0, category: "" });
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mini Bank App</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={newTransaction.description}
+          onChange={handleInputChange}
+        />
+        <input
+          type="number"
+          name="amount"
+          placeholder="Amount"
+          value={newTransaction.amount}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Category"
+          value={newTransaction.category}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add Transaction</button>
+      </form>
+      <input
+        type="text"
+        placeholder="Search by description"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredTransactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.description}</td>
+              <td>${transaction.amount}</td>
+              <td>{transaction.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
